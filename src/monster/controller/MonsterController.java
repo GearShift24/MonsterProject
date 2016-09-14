083b3a48065c8a043c995ed46257c70c203b2b06
GearShift24 <jwri6847@740s-pl-369.local>
2016-09-14 08:19:03 -0600

    Wrote the runner class

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -3,4 +3,9 @@ package monster.controller;
 public class MonsterController
 {
 
+	public static void main (String [] args)
+	{
+		MonsterController monsterAppController = new MonsterController();
+		monsterAppController.start();
+	}
 }
