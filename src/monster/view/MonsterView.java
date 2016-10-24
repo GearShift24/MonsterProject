9a4d5035169659ae249651eb080f41ed9f2b4350
GearShift24 <jwri6847@740s-pl-369.local>
2016-10-24 09:14:20 -0600

    d

--- src/monster/view/MonsterView.java
+++ src/monster/view/MonsterView.java
@@ -1,6 +1,22 @@
 package monster.view;
 
+import javax.swing.JOptionPane;
+
 public class MonsterView
 {
 
+	public void displayMessage(String message)
+	{
+		JOptionPane.showMessageDialog(null,message);
+		
+	}
+	
+	public String collectResponse(String question)
+	{
+		String answer = "";
+		
+		answer = JOptionPane.showInputDialog(null, question);
+		
+		return answer;
+	}
 }
